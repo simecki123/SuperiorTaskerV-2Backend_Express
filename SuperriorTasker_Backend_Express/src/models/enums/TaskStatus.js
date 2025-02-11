3143bb012042f0ccc07c2e08f1104e3bf26bd109
Sime Roncevic <sroncevic19@gmail.com>
2025-02-11 13:38:22 +0100

    initial commit with first setup

---
+++ SuperriorTasker_Backend_Express/src/models/enums/TaskStatus.js
@@ -0,0 +1,10 @@
+const TaskStatus = {
+    TODO: 'TODO',
+    IN_PROGRESS: 'IN_PROGRESS',
+    DONE: 'DONE',
+    ARCHIVED: 'ARCHIVED'
+};
+
+Object.freeze(TaskStatus);
+
+module.exports = TaskStatus;
